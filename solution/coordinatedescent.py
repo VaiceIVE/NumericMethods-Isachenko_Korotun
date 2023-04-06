1711f39a8a9e9b7c6959ea58e9f302a783fe65d2
Isachenko Ivan <vanisachenko@gmail.com>
2023-04-06 18:42:28 +0600

    smoker coordinatedescent

---
+++ solution/coordinatedescent.py
@@ -0,0 +1,60 @@
+import numpy as np
+from math import exp
+from typing import Callable, List
+
+import numpy.linalg.linalg
+import scipy.optimize
+from scipy.optimize import minimize
+import sympy as sp
+
+x, y = sp.symbols('x y')
+f = 30 - (x - 2) * sp.exp(-x + 2) - (y - 1) * sp.exp(-y + 1)
+
+
+def funcinx(func, x, vars):
+    for i, var in enumerate(vars):
+        func = sp.N(func.subs(var, x[i]))
+    return func
+
+
+
+def gradinx(grads, x, vars):
+    reslist = list()
+    for i, grad in enumerate(grads):
+        for j, var in enumerate(vars):
+            grad = sp.N(grad.subs(var, x[j]))
+        reslist.append(float(grad))
+    return np.array(reslist)
+
+
+def coordinate_descent(func,
+                       x0: List[float],
+                       eps: float = 0.0001,):
+    k = 0
+    x_points = [x0]
+    grad = list()
+    vars = func.free_symbols
+    for var in vars:
+        grad.append(sp.diff(func, var))
+    grad = np.array(grad)
+    print(gradinx(grad, x_points[k], vars))
+    norm = numpy.linalg.norm(gradinx(grad, x_points[k], vars))
+    print(norm)
+    print("Start")
+    while norm > eps:
+        sk = -gradinx(grad, x_points[k], vars)/norm
+        print("SK = ", sk)
+        l = sp.Symbol('l')
+        def functomin(lamb):
+            nonlocal sk
+            subfunc = func
+            for i, var in enumerate(vars):
+                subfunc = subfunc.subs(var, x_points[k][i])
+            return np.array(subfunc + lamb * funcinx(func, sk, vars))
+        res = scipy.optimize.minimize(functomin, 0, method='COBYLA')
+        x_points.append(x_points[k] + res.x[0] * sk)
+        norm = numpy.linalg.linalg.norm(gradinx(grad, x_points[k], list(vars)))
+        k += 1
+    return x_points[len(x_points) - 1]
+
+print(coordinate_descent(f, [12, 12]))
